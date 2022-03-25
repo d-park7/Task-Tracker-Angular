db601861ee2cb971db365642ad61cc3e0b7567aa
d-park7 <davidparkdp450@gmail.com>
2022-03-25 17:13:39 -0500

    Creating express api and integrating mongodb

--- node-rest-api/server.js
+++ node-rest-api/server.js
@@ -3,15 +3,36 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const mongoose = require('mongoose')
+const cors = require('cors')
 const express = require('express')
+const Tasks = require('./Tasks')
+
 const app = express()
-const Task = require('./Tasks')
+  .use(cors())
 
 mongoose.connect(process.env.DATABASE_URL, () => {
-  console.log("connected to mongodb")
-}, e => console.error(e))
-
-
-new Task({ })
+  console.log('Connected to database')
+})
 
 app.listen(3000)
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Tasks.find({})
+  console.log(tasks[1].text)
+  res.json({ text: task.text, day: task.day, reminder: task.reminder })
+})
+
+async function createTask() {
+  try {
+    const task = await Tasks.create({
+      text: "testTask2",
+      day: "testDay2",
+      reminder: true
+    })
+    //console.log(task)
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+//createTask()
